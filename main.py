29eb997b9a09ccbf12c37c231f0fce39e9a1b276
sudoskys <coldlando@hotmail.com>
2024-01-12 17:03:54 +0800

    feat(main): add main.py with server configuration and logger setup
    This commit adds the main.py file, which contains the server configuration
    settings and logger setup. The server settings include the server port and
    host, while the logger is configured to output logs to the console and a log
    file. Additionally, a ServerSetting class is defined with model validation for
    the server host. The commit also sets the HTTP and HTTPS proxy environment
    variables and runs the server using uvicorn.

---
+++ main.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2023/12/15 上午1:35
+# @Author  : sudoskys
+# @File    : main.py
+import os
+import sys
+
+import uvicorn
+from dotenv import load_dotenv
+from loguru import logger
+from pydantic import model_validator
+from pydantic_settings import BaseSettings
+
+import app
+
+load_dotenv()
+logger.remove()
+logger.add(sys.stderr, level="INFO", colorize=True, enqueue=True)
+logger.add(sink='run.log', format="{time} - {level} - {message}", level="INFO", rotation="100 MB", enqueue=True)
+
+
+class ServerSetting(BaseSettings):
+    server_port: int = 10011
+    server_host: str = "127.0.0.1"
+
+    @model_validator(mode="after")
+    def check(self):
+        if self.server_host.startswith("https://"):
+            self.server_host = self.server_host.replace("https://", "")
+            logger.warning("Wrong HOST, should remove the prefix https://")
+        if self.server_host.startswith("http://"):
+            self.server_host = self.server_host.replace("http://", "")
+            logger.warning("Wrong HOST, should remove the prefix http://")
+        assert isinstance(self.server_host, str)
+        assert isinstance(self.server_port, int)
+        return self
+
+
+setting = ServerSetting()
+logger.info(f"Docs: http://{setting.server_host}:{setting.server_port}/docs")
+os.environ['HTTP_PROXY'] = 'http://127.0.0.1:7897'
+os.environ['HTTPS_PROXY'] = 'http://127.0.0.1:7897'
+uvicorn.run(app.app, host=setting.server_host, port=setting.server_port)
